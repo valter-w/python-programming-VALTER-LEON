2ef82a3a10776958b5b74499deafb4940520935c
Valter Wierzba <valter.wierzba@gmail.com>
2023-09-21 18:36:23 +0200

    Interpreting data in datapoints.txt

--- Labs/Lab_2/program.py
+++ Labs/Lab_2/program.py
@@ -15,6 +15,31 @@ print(f"{data_points_path = }")
 with open(data_points_path) as file:
     lines = file.readlines()
 
+# Manage file contents outside with statement above in order to 
+# close the file asap.
+
+# remove "\n" at end of each line
+lines = [line.rstrip() for line in lines]
+
+# remove head line
+lines = lines[1:]
+
+# each item will contain data about a pokemon
+measurements = []
+
+for line in lines:
+    line_data = line.split(", ")
+    width = float(line_data[0])
+    height = float(line_data[1])
+    label = int(line_data[2])
+    measurement = {
+        "width": width,
+        "height": height,
+        "label": label
+    }
+    measurements.append(measurement)
+    print(f"{measurement = }")
+
 # Check if there were any lines read from datapoints.txt
 print(f"{len(lines) = }")
 
